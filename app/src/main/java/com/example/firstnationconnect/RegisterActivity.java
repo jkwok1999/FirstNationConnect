25c3ae3f05517006b7255abcbf392c570535ce6e
William Liu <william_liu01@hotmail.com>
2021-07-15 23:10:31 +1000

    SetupActivity pree much complete (need to be edited but still ok)

--- app/src/main/java/com/example/firstnationconnect/RegisterActivity.java
+++ app/src/main/java/com/example/firstnationconnect/RegisterActivity.java
@@ -26,10 +26,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText newEmail, newPassword, registerFirstName, registerLastName, registerUsername, registerAge;
+    private EditText newEmail, newPassword, newPasswordConfirm;
     private Button createAccount;
-    private CheckBox registerCheckBox;
     private ProgressBar registerProgressBar;
+    private CheckBox registerCheckbox;
 
 
     private static final String TAG = "RegisterActivity";
@@ -43,15 +43,12 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
         newEmail = findViewById(R.id.newEmail);
         newPassword = findViewById(R.id.newPassword);
-        registerFirstName = findViewById(R.id.registerFirstName);
-        registerLastName = findViewById(R.id.registerLastName);
-        registerUsername = findViewById(R.id.registerUsername);
-        registerAge = findViewById(R.id.registerAge);
+        newPasswordConfirm = findViewById(R.id.newPasswordConfirm);
         createAccount = findViewById(R.id.createAccount);
         createAccount.setOnClickListener(this);
 
-        registerCheckBox = findViewById(R.id.registerCheckBox);
         registerProgressBar = findViewById(R.id.registerProgressBar);
+        registerCheckbox = findViewById(R.id.registerCheckBox);
 
         mAuth = FirebaseAuth.getInstance();
         firestoreDB = FirebaseFirestore.getInstance();
@@ -69,26 +66,27 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             FirebaseUser user = mAuth.getCurrentUser();
             String email = newEmail.getText().toString().trim();
             String password = newPassword.getText().toString().trim();
-            String firstName = registerFirstName.getText().toString().trim();
-            String lastName = registerLastName.getText().toString().trim();
-            String username = registerUsername.getText().toString().trim();
+            String confirmPassword = newPasswordConfirm.getText().toString().trim();
+//            String firstName = registerFirstName.getText().toString().trim();
+//            String lastName = registerLastName.getText().toString().trim();
+//            String username = registerUsername.getText().toString().trim();
             boolean loginValid = true;
-            if (registerAge.getText().toString().trim().isEmpty()){
-                registerAge.setError("Age is required!");
-                loginValid = false;
-            }
-            if (registerUsername.getText().toString().trim().isEmpty()){
-                registerUsername.setError("Username is required!");
-                loginValid = false;
-            }
-            if (firstName.isEmpty()) {
-                registerFirstName.setError("First name is required!");
-                loginValid = false;
-            }
-            if (lastName.isEmpty()) {
-                registerLastName.setError("Last name is required!");
-                loginValid = false;
-            }
+//            if (registerAge.getText().toString().trim().isEmpty()){
+//                registerAge.setError("Age is required!");
+//                loginValid = false;
+//            }
+//            if (registerUsername.getText().toString().trim().isEmpty()){
+//                registerUsername.setError("Username is required!");
+//                loginValid = false;
+//            }
+//            if (firstName.isEmpty()) {
+//                registerFirstName.setError("First name is required!");
+//                loginValid = false;
+//            }
+//            if (lastName.isEmpty()) {
+//                registerLastName.setError("Last name is required!");
+//                loginValid = false;
+//            }
             if (email.isEmpty()) {
                 newEmail.setError("Email is required!");
                 loginValid = false;
@@ -104,72 +102,74 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
             if (password.length() < 6) {
                 newPassword.setError("Password must be at least 6 characters!");
                 loginValid = false;
-                return;
+            }
+            if(!password.equals(confirmPassword)) {
+                newPasswordConfirm.setError("Password does not match!");
+                loginValid = false;
             }
 
-            int age = Integer.parseInt(registerAge.getEditableText().toString());
-            if (age < 16){
-                registerAge.setError("User must be 16 years or older!");
-                loginValid = false;
-            }
-            if (age > 110){
-                registerAge.setError("Please enter valid age!");
-                loginValid = false;
-            }
-            if (!registerCheckBox.isChecked()) {
-                registerCheckBox.setError("Please agree to Terms and Conditions");
+//            int age = Integer.parseInt(registerAge.getEditableText().toString());
+//            if (age < 16){
+//                registerAge.setError("User must be 16 years or older!");
+//                loginValid = false;
+//            }
+//            if (age > 110){
+//                registerAge.setError("Please enter valid age!");
+//                loginValid = false;
+//            }
+            if (!registerCheckbox.isChecked()) {
+                registerCheckbox.setError("Please agree to Terms and Conditions");
                 loginValid = false;
             } else {
-                registerCheckBox.setError(null);
+                registerCheckbox.setError(null);
             }
             if (loginValid == true) {
 
                 registerProgressBar.setVisibility(View.VISIBLE);
-                registerCheckBox.setError(null);
+                registerCheckbox.setError(null);
                 mAuth.createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    User user = new User(firstName, lastName, email, username, age);
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-
-
-
-                                    /*FirebaseDatabase.getInstance().getReference("Users")
-                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {*/
-                                    firestoreDB.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(user)
-                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                FirebaseUser user = mAuth.getCurrentUser();
-                                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                                        .setDisplayName(username).build();
-                                                user.updateProfile(profileUpdates);
-                                                Toast.makeText(RegisterActivity.this, "Account Created Successfully",
-                                                        Toast.LENGTH_LONG).show();
-                                                registerProgressBar.setVisibility(View.GONE);
-                                                finish();
-                                                mAuth.signOut();
-                                            } else {
-                                                // If sign in fails, display a message to the user.
-                                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                                Toast.makeText(RegisterActivity.this, "Account Creation Failed. Please Try Again",
-                                                        Toast.LENGTH_LONG).show();
-                                                registerProgressBar.setVisibility(View.GONE);
-                                                updateUI(null);
-                                            }
-                                        }
-                                    });
+                                    Intent setupIntent = new Intent(RegisterActivity.this, SetupActivity.class);
+                                    startActivity(setupIntent);
+                                    finish();
+//                                    User user = new User(firstName, lastName, email, username, age);
+//                                    // Sign in success, update UI with the signed-in user's information
+//                                    Log.d(TAG, "createUserWithEmail:success");
+//
+//                                    firestoreDB.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid()).set(user)
+//                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                        @Override
+//                                        public void onComplete(@NonNull Task<Void> task) {
+//                                            if (task.isSuccessful()) {
+//                                                FirebaseUser user = mAuth.getCurrentUser();
+//                                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+//                                                        .setDisplayName(username).build();
+//                                                user.updateProfile(profileUpdates);
+//                                                Toast.makeText(RegisterActivity.this, "Account Created Successfully",
+//                                                        Toast.LENGTH_LONG).show();
+//                                                registerProgressBar.setVisibility(View.GONE);
+//                                                finish();
+//                                                mAuth.signOut();
+//                                            } else {
+//                                                // If sign in fails, display a message to the user.
+//                                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+//                                                Toast.makeText(RegisterActivity.this, "Account Creation Failed. Please Try Again",
+//                                                        Toast.LENGTH_LONG).show();
+//                                                registerProgressBar.setVisibility(View.GONE);
+//                                                updateUI(null);
+//                                            }
+//                                        }
+//                                    });
 
                                 } else {
                                     Toast.makeText(RegisterActivity.this, "Account Creation Failed. Please Try Again",
                                             Toast.LENGTH_SHORT).show();
                                     registerProgressBar.setVisibility(View.GONE);
                                 }
+                                registerProgressBar.setVisibility(View.GONE);
                             }
                             private void updateUI(FirebaseUser currentUser) {
 
