401cb2e15e84af83f6982bb71a34fc948b38b552
jkwok1999@gmail.com <jkwok1999@gmail.com>
2021-07-15 23:23:11 +1000

    Made user variables private, added getters and setters.

--- app/src/main/java/com/example/firstnationconnect/User.java
+++ app/src/main/java/com/example/firstnationconnect/User.java
@@ -1,12 +1,13 @@
 package com.example.firstnationconnect;
 
 public class User {
-    public String firstName, lastName, email, username;
-    public int age;
+    private String firstName, lastName, email, username;
+    private int age;
 
     public User() {
 
     }
+
     public User(String firstName, String lastName, String email, String username, int age){
         this.firstName = firstName;
         this.lastName = lastName;
@@ -15,4 +16,43 @@ public class User {
         this.age = age;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
