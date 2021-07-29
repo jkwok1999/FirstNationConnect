e1e3cd444a5d17b223cc5268c63dbdb5b99c7cfb
William Liu <william_liu01@hotmail.com>
2021-07-29 22:20:58 +1000

    edit lil

--- app/src/main/java/com/example/firstnationconnect/SurveyActivity.java
+++ app/src/main/java/com/example/firstnationconnect/SurveyActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -23,6 +24,7 @@ public class SurveyActivity extends AppCompatActivity {
     private FirebaseFirestore firestoreDB;
     private Button btSubmitSurvey;
     private RadioGroup rgQuestionOne, rgQuestionTwo;
+    private TextView tvQuestionOne;
     //    private RadioButton rbQuestion1StronglyAgree, rbQuestion1Agree, rbQuestion1Neutral,
 //            rbQuestion1Disagree, rbQuestion1StronglyDisagree;
 //    private RadioButton rbQuestion2StronglyAgree, rbQuestion2Agree, rbQuestion2Neutral,
@@ -37,6 +39,7 @@ public class SurveyActivity extends AppCompatActivity {
 
         firestoreDB = FirebaseFirestore.getInstance();
 
+        tvQuestionOne = findViewById(R.id.tvQuestionOne);
         rgQuestionOne = findViewById(R.id.rgQuestionOne);
         rgQuestionTwo = findViewById(R.id.rgQuestionTwo);
 
@@ -59,32 +62,40 @@ public class SurveyActivity extends AppCompatActivity {
         btSubmitSurvey.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int radioId1 = rgQuestionOne.getCheckedRadioButtonId();
-
-                radioButton1 = findViewById(radioId1);
-
-                String question1Response = radioButton1.getText().toString();
-
-                DocumentReference docRef = firestoreDB.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
-                docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        User user = documentSnapshot.toObject(User.class);
-                        String username = user.getUsername();
-                        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-                        Survey surveyResults = new Survey(username, userId, question1Response);
-                        //add more question attributes other than 1
-
-                        firestoreDB.collection("Survey").document("Survey - " + FirebaseAuth.getInstance().getCurrentUser().getUid()).set(surveyResults);
-                        Toast.makeText(SurveyActivity.this, "Survey Complete",
-                                Toast.LENGTH_SHORT).show();
-                        finish();
-                    }
-                });
+                tvQuestionOne.setTextColor(getResources().getColor(R.color.black));
+                boolean surveyValid = true;
 
 
+                if(rgQuestionOne.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(SurveyActivity.this, "Please enter all required survey fields",
+                            Toast.LENGTH_SHORT).show();
+                    tvQuestionOne.setTextColor(getResources().getColor(R.color.colorRed));
+                    surveyValid = false;
+                } else {
+                    int radioId1 = rgQuestionOne.getCheckedRadioButtonId();
+
+                    radioButton1 = findViewById(radioId1);
+
+                    String question1Response = radioButton1.getText().toString();
+
+                    DocumentReference docRef = firestoreDB.collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+                    docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                        @Override
+                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                            User user = documentSnapshot.toObject(User.class);
+                            String username = user.getUsername();
+                            String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                            Survey surveyResults = new Survey(username, userId, question1Response);
+                            //add more question attributes other than 1
+
+                            firestoreDB.collection("Survey").document("Survey - " + FirebaseAuth.getInstance().getCurrentUser().getUid()).set(surveyResults);
+                            Toast.makeText(SurveyActivity.this, "Survey Complete",
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+                }
             }
         });
     }
